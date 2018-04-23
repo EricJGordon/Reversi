c31651130803d681d21e437437158590e345d995
Starok <rokas.stasiskis@ucdconnect.ie>
2018-04-23 23:41:33 +0100

    bool problem and fopen problem

--- Assignment3Code/main.c
+++ Assignment3Code/main.c
@@ -9,34 +9,27 @@
 
 int main()
 {
+    FILE *ScoreSheet;
     // Variables declaration
+
     player player1= {"player1", NONE,0};
     player player2= {"player2", NONE,0};
     disk board[SIZE][SIZE];
     bool cont = true;
     initializePlayers(&player1, &player2);
-    printf("\n\n%s's disks are represented by \"B\", %s's disks are represented by \"W\" and available moves are represented by \"o\".\n", player1.name, player2.name); //Info for players
+    printf("%s's disks are represented by \"B\" \n%s's disks are represented by \"W\" \nAvailable moves are represented by \"o\".\n", player1.name, player2.name); //Info for players
 
     initializeBoard(board);
-
-    //testing sample board state
-
-    //computePositions(board, player2, &cont);
-
-    //computePositions(board, player1, &cont);
-
-
-
-
-    while (cont = true)
-        {
+    ScoreSheet = fopen("./Assignment3/Assignment3Code/Score.txt","w+");
+    fprintf(ScoreSheet, "Player1 %s, points: %d \nPlayer2 %s, points: %d \nThe winner is ", player1.name, player1.points, player2.name, player2.points);
+    while (cont ==true){
 
         computePositions(board, player1, &cont);
         printBoard(board);
         scores(board, &player1, &player2);
 
 
-        if (cont = false)
+        if (cont == false)
             break;
 
         playerMove(board, player1);
@@ -47,14 +40,14 @@ int main()
         scores(board, &player1, &player2);
 
 
-        if (cont = false)
+        if (cont == false)
             break;
 
         playerMove(board, player2);
         refreshBoard(board);
         }
 
-
     printEndScreen(player1, player2);
+
     return 0;
 }
